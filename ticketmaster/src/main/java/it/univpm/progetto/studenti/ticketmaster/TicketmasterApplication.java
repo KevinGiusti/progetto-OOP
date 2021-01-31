5d5992b027180c7d9e9e964532b1400622546d61
kejvin <s1093119@studenti.univpm.it>
2021-01-31 18:31:01 +0100

    Start

---
+++ ticketmaster/src/main/java/it/univpm/progetto/studenti/ticketmaster/TicketmasterApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto.studenti.ticketmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterApplication.class, args);
+	}
+
+}
---
+++ ticketmaster/src/test/java/it/univpm/progetto/studenti/ticketmaster/TicketmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto.studenti.ticketmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
