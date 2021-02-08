ac74711fa78e384a54791ba3fd995f7c2c2b474e
RoccoAnzivino <s1092774@studenti.univpm.it>
2021-02-08 06:01:01 +0100

    0.1.3.0 - #Creata la classe Eventi nel package model. Creato il package parse con la classe EventiParser al suo interno. Modificata e ultimata la classe ChiamataEventi nel package api

--- ticketmaster/src/main/java/it/univpm/progetto/studenti/ticketmaster/api/ChiamataEventi.java
+++ ticketmaster/src/main/java/it/univpm/progetto/studenti/ticketmaster/api/ChiamataEventi.java
@@ -8,9 +8,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Vector;
 
+import it.univpm.progetto.studenti.ticketmaster.model.Eventi;
+import it.univpm.progetto.studenti.ticketmaster.parser.EventiParser;
+
 public class ChiamataEventi {
 	
-	public static /*Vector<Eventi>*/ void  chiamata(String stato) {
+	public static Vector<Eventi> chiamata(String stato) {
+		
+		Vector<Eventi> listaEventi = new Vector<Eventi>();
 		
 		try {
 			
@@ -18,12 +23,15 @@ public class ChiamataEventi {
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String json = input.readLine();
+			EventiParser eP = new EventiParser();
+			listaEventi = eP.parse(json);
 			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return listaEventi;
 	
 	}
 
