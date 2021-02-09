27671993cd9819d08ec289130a05e346c79aa7b5
RoccoAnzivino <s1092774@studenti.univpm.it>
2021-02-09 06:35:11 +0100

    0.1.6.0 - #Creati i package resources e scanner, dove sono stati inseriti rispettivamente il file 'Stati.csv' e la lettura del file

---
+++ ticketmaster/src/main/java/it/univpm/progetto/studenti/ticketmaster/scanner/StatiScanner.java
@@ -0,0 +1,30 @@
+package it.univpm.progetto.studenti.ticketmaster.scanner;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class StatiScanner {
+
+	public static Vector<String> getStati() {
+		
+		Vector<String> statiVect = new Vector<String>();
+		
+		try {
+			
+			Scanner fileStati = new Scanner(new BufferedReader(new FileReader("resources/Stati.csv")));
+			
+			while (fileStati.hasNext())
+				statiVect.add(fileStati.next());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return statiVect;
+		
+	}
+	
+}
