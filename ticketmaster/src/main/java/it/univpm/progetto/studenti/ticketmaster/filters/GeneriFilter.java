f55832f30dc324912a7a3cf7af7acd7696d9ed0d
kejvin <s1093119@studenti.univpm.it>
2021-02-12 02:57:54 +0100

    0.1.14.0 -# Creazione e popolazione classe GeneriFilter

---
+++ ticketmaster/src/main/java/it/univpm/progetto/studenti/ticketmaster/filters/GeneriFilter.java
@@ -0,0 +1,40 @@
+package it.univpm.progetto.studenti.ticketmaster.filters;
+
+import java.util.Vector;
+
+import it.univpm.progetto.studenti.ticketmaster.model.Eventi;
+
+/**
+ * 
+ * Classe che effettua un filtro sui generi
+ * 
+ * @author KejvinSkiti
+ *
+ */
+public class GeneriFilter {
+
+	/**
+	 * 
+	 * Metodo che filtra per generi gli eventi
+	 * 
+	 * @param genere Definisce il genere utilizzato per filtrare gli eventi
+	 * @param eventiDaFiltrare Tutti gli eventi sottoposti al filtro
+	 * @return eventiFiltrati Lista degli eventi dopo il filtraggio
+	 * @see it.univpm.progetto.studenti.ticketmaster.model.Eventi
+	 */
+	public static Vector<Eventi> filterByGenre(String genere, Vector<Eventi> eventiDaFiltrare) {
+		
+		Vector<Eventi> eventiFiltrati = new Vector<Eventi>();
+		
+		for (Eventi eventiTemp : eventiDaFiltrare) {
+			
+			if(genere.equals(eventiTemp.getGenere()))
+				eventiFiltrati.add(eventiTemp);
+			
+		}
+		
+		return eventiFiltrati;
+		
+	}
+	
+}
